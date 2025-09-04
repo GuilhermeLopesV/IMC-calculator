977c33f2762d3611eb330a0745e651febdc7e726
Guilherme <guilhermelopesvieira0@gmail.com>
2025-09-04 10:25:57 -0300

    second commit

--- imc.py
+++ imc.py
@@ -1,37 +1,41 @@
 while True:
 
-    name = input('Digite seu nome: ')
-    height = float(input('Digite sua altura: '))
-    weight = int(input('Digite seu peso: '))
-    imc = weight / (height * height)
+    try:
+        name = input('Digite seu nome: ').capitalize()
+        height = float(input('Digite sua altura: '))
+        weight = int(input('Digite seu peso: '))
+        imc = weight / (height * height)
+        your_weight = f'Seu IMC é {imc:.2f}'
+        print('Calculando...')
+    except ValueError:
+        print('Valor digitado incorreto.')
+        continue
+    except ZeroDivisionError:
+        print('Peso e altura devem ser maiores que zero.')
+        continue
 
-    if imc <= 18.5:
+    if imc <= 18.4:
         print(f'{name} você está abaixo do peso')
-        print(f'Seu peso é {imc:.2f}')
 
-    elif 18.6 <= imc < 24.9:
+    elif 18.5 <= imc < 24.9:
         print(f'{name} você está com peso normal')
-        print(f'Seu peso é {imc:.2f}')
 
     elif 25 <= imc < 29.9:
         print(f'{name} você está com pré-obesidade')
-        print(f'Seu peso é {imc:.2f}')
 
     elif 30 <= imc < 34.9:
         print(f'{name} você está com obesidade, grau I')
-        print(f'Seu peso é {imc:.2f}')
 
     elif 35 <= imc < 39.9:
         print(f'{name} você está com obesidade, grau II')
-        print(f'Seu peso é {imc:.2f}')
 
     elif imc > 40:
         print(f'{name} você está com obesidade, grau III')
-        print(f'Seu peso é {imc:.2f}')
 
     else:
-        print("Peso e altura devem ser maiores que zero.")
+        print('Isso não deveria acontecer')
 
+    print(your_weight)
     close = input('Digite (encerrar) ou (e) para encerrar o programa: ').lower()
     if close == 'encerrar' or close == 'e':
         print("Programa encerrado.")
